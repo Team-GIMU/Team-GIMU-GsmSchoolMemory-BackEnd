fda241324c90d8e49cb3bbb241de3ec1325b5342
ta2ye0n <tyeon42051@naver.com>
2025-04-14 14:06:08 +0900

    init

---
+++ src/main/java/com/gimu/gsm/GsmApplication.java
@@ -0,0 +1,13 @@
+package com.gimu.gsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsmApplication.class, args);
+    }
+
+}
