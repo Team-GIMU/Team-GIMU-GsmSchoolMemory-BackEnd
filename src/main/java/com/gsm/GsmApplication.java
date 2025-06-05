e00f96d3cb9ef4227600030db76a209f7ee8000c
m2ri1 <parkmiri0722@gmail.com>
2025-06-05 14:57:50 +0900

    update GsmApplication location

--- src/main/java/com/gsm/GsmApplication.java
+++ src/main/java/com/gsm/GsmApplication.java
@@ -1,4 +1,4 @@
-package com.gimu.gsm;
+package com.gsm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gsm/GsmApplicationTests.java
+++ src/test/java/com/gsm/GsmApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gimu.gsm;
+package com.gsm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
