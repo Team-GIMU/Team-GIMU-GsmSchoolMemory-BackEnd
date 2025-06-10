7f94d46273ec031609efaababe98b0cb61469d85
ta2ye0n <tyeon42051@naver.com>
2025-06-10 11:16:07 +0900

    dto

---
+++ src/main/java/com/gsm/domain/user/presentation/dto/request/RoleChangeRequest.java
@@ -0,0 +1,13 @@
+package com.gsm.domain.user.presentation.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RoleChangeRequest {
+    private String email;
+}
