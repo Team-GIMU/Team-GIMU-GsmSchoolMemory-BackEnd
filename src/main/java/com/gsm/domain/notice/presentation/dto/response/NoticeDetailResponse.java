22467ed1fd23a2a2c4a0b6338e2edd95e42051af
ta2ye0n <tyeon42051@naver.com>
2025-06-10 09:17:12 +0900

    dto

---
+++ src/main/java/com/gsm/domain/notice/presentation/dto/response/NoticeDetailResponse.java
@@ -0,0 +1,22 @@
+package com.gsm.domain.notice.presentation.dto.response;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NoticeDetailResponse {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime editedDate;
+}
