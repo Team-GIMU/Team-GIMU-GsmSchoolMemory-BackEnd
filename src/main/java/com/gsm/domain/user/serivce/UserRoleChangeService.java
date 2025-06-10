c49e578c19a36f0e10a72344d1c6cdff09a905e3
ta2ye0n <tyeon42051@naver.com>
2025-06-10 11:16:01 +0900

    service

---
+++ src/main/java/com/gsm/domain/user/serivce/UserRoleChangeService.java
@@ -0,0 +1,25 @@
+package com.gsm.domain.user.serivce;
+
+import com.gsm.domain.auth.exception.UserNotFoundException;
+import com.gsm.domain.user.entity.User.User;
+import com.gsm.domain.user.enums.Role;
+import com.gsm.domain.user.presentation.dto.request.RoleChangeRequest;
+import com.gsm.domain.user.repository.UserRepository;
+import com.gsm.global.annotation.RollbackService;
+import lombok.RequiredArgsConstructor;
+
+@RollbackService
+@RequiredArgsConstructor
+public class UserRoleChangeService {
+    private final UserRepository userRepository;
+
+    public void execute(RoleChangeRequest roleChangeRequest) {
+
+        String email = roleChangeRequest.getEmail();
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        user.updateRole(Role.ROLE_STUDENT);
+    }
+}
